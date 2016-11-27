d963254f13655d258c3f6c357e41df1a3a2dc68e
byung-u <iam.byungwoo@gmail.com>
2016-11-27 22:11:29 +0900

    Add naver 실시간 검색 순위 1~10 조회

---
+++ test/t002_naver_search_rank.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3.5
+# -*- coding: utf-8 -*-
+
+import re
+from requests import *
+from bs4 import BeautifulSoup
+
+def t002():
+    url = "http://www.naver.com/"
+    r = get(url)
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    options = soup.find_all('option')
+    realtime_rank = []
+
+    for opt in options:
+        opt = str(opt)
+        m = re.search('>\d+위:', opt)
+        if (m == None):
+            continue
+        idx = opt.find(">")
+        opt = opt[idx+1:]
+        realtime_rank.append(opt)
+
+    if (len(realtime_rank) == 0):
+        return -1
+
+    print("[TEST002][OK]")
+    print('\t', realtime_rank)
+    return 0
--- test/test.py
+++ test/test.py
@@ -4,6 +4,7 @@ import sys
 import configparser
 
 from t001_realstate import t001
+from t002_naver_search_rank import t002
 
 if __name__ == '__main__':
     ok = 0
@@ -18,6 +19,11 @@ if __name__ == '__main__':
     request_url = '%s?LAWD_CD=%s&DEAL_YMD=%s&serviceKey=%s' % (url, 11440, 201611, svc_key)
 
     if (t001(request_url) == 0):
+        ok += 1
+    else:
+        nok += 1
+
+    if (t002() == 0):
         ok += 1
     else:
         nok += 1
