056a071c7d788e30697995ea332dde7dcd26282e
byung-u <iam.byungwoo@gmail.com>
2016-12-13 15:54:13 +0900

    Fix with realstate trade

--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.5
 # -*- coding: utf-8 -*-
 import sys
 import configparser
@@ -9,6 +9,7 @@ from t004_seoul_dust_level import t004
 from t005_naver_search_rank import t005
 from t006_postal_code import t006
 from t007_naver_translate import t007
+from t008_realstate_trade import t008
 
 if __name__ == '__main__':
     ok = 0
@@ -20,7 +21,7 @@ if __name__ == '__main__':
     config.readfp(open('../bot.ini'))
 
     url = config.get('TOKEN', 'apt_rent_url')
-    svc_key = config.get('TOKEN', 'apt_rent_key', raw=True)
+    svc_key = config.get('TOKEN', 'apt_key', raw=True)
     request_url = '%s?LAWD_CD=%s&DEAL_YMD=%s&serviceKey=%s' % (url, 11440, 201611, svc_key)
 
     if (t001(request_url) == 0):
@@ -54,6 +55,14 @@ if __name__ == '__main__':
     else:
         nok += 1
 
+    url = config.get('TOKEN', 'apt_trade_url')
+    request_url = '%s?LAWD_CD=%s&DEAL_YMD=%s&serviceKey=%s' % (url, 11440, 201611, svc_key)
+    if (t008(request_url) == 0):
+        ok += 1
+    else:
+        nok += 1
+
+ 
     result = "\n\nOK:%d, PASS:%d, NOK:%d" % (ok, not_check, nok)
     print(result)
 
